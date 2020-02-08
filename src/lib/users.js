8d955e2d903fa668274b17d9016f938f37f8fbd3
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2020-02-08 13:38:09 +0200

    fixed eslint eerros

--- src/lib/users.js
+++ src/lib/users.js
@@ -16,18 +16,10 @@ const users = new mongoose.Schema({
 
 users.pre('save', async function(){
   if (!users.username) {
-  this.password = await bcrypt.hash(this.password, 10);
-  };
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 });
-// users.statics.authenticateBasic = async function(auth) {
-//   console,log('++++++++++++++++++++',auth)
-//   return this.findOne({username:auth.user})
-//     .then(user=>{
-//       console,log(user)
-//       return bcrypt.compare(auth.pass, user.password)
-//     .then(valid => valid ? user : null);
-//     });
-//   }
+
 users.statics.authenticateBasic = function(auth) {
   return this.findOne({username:auth.username})
     .then(user => user.passCompare(auth.password))
@@ -38,31 +30,31 @@ users.methods.passCompare = function(password) {
     .then(valid => valid ? this : null);
 };
 users.methods.generateToken = function(user) {
-    let token = jwt.sign({ username: user.username}, process.env.SECRET);
-    return token;
-  }
+  let token = jwt.sign({ username: user.username}, process.env.SECRET);
+  return token;
+};
 
-  // users.save = async function(record) {
-  // if (!db[record.username]) {
-  //     record.password = await bcrypt.hash(record.password, 5);
+// users.save = async function(record) {
+// if (!db[record.username]) {
+//     record.password = await bcrypt.hash(record.password, 5);
 
-  //     db[record.username] = record;
-  //     record.save()
-  //     return record;
-  //   }
+//     db[record.username] = record;
+//     record.save()
+//     return record;
+//   }
 
-  //   return Promise.reject();
-  // }
+//   return Promise.reject();
+// }
 
-  // users.authenticateBasic = async function(user,pass) {
-  //   let valid = await bcrypt.compare(pass, db[user].password);
-  //   return valid ? db[user] : Promise.reject();
-  // }
+// users.authenticateBasic = async function(user,pass) {
+//   let valid = await bcrypt.compare(pass, db[user].password);
+//   return valid ? db[user] : Promise.reject();
+// }
 
-  // users.generateToken = function(user) {
-  //   let token = jwt.sign({ username: user.username}, SECRET);
-  //   return token;
-  // }
+// users.generateToken = function(user) {
+//   let token = jwt.sign({ username: user.username}, SECRET);
+//   return token;
+// }
 
 users.statics.list =  async function(){
   let results = await this.find({});
